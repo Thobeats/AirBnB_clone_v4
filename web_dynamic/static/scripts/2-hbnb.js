ac6ccd0e12b45e120c85f7e83ea3774803b7dfe9
Iyanu Dayisi <tobiy23@gmail.com>
2024-06-02 01:20:47 +0100

    web dynamic

---
+++ web_dynamic/static/scripts/2-hbnb.js
@@ -0,0 +1,16 @@
+/**This is the beginning of greatness */
+
+$(document).ready(()=>{
+    // get the API status
+   $.get("http://0.0.0.0:5001/api/v1/status/", (data) => {
+        if (data.status == "OK"){
+            console.log(data)
+
+            $("div#api_status").addClass('available')
+        }
+        else
+        {
+            $("div#api_status").removeClass('available')
+        }
+   })
+})
